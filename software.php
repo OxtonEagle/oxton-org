0c527b151273dc671ad684129f3793b1e0c92288
tjohnson-vldb <tjohnson@vldbsolutions.com>
2024-04-19 13:15:31 +0100

    Added files

---
+++ software.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+    <head>
+        <title>Tom Johnson</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel= "stylesheet" type = "text/css" href= "css/style.css?v=13">
+        <link rel="icon" href="images/tj.png" type="image/x-icon">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    </head>
+    
+    <body>
+
+        <!-- header -->
+        <ul class="topnav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a class="active">Software</a></li>
+            <li><a href="qualifications.php">Qualifications</a></li>
+            <li><a href="cv.php">CV</a></li>
+            <li class="right"><a style="color:rgb(94,144,253);">TOM JOHNSON</a></li>
+        </ul> 
+        
+<!------------------------------------ main body ---------------------------------------->
+
+        <div class="banner" style="background-image:url('images/software.jpg');">
+            <div class="shade_box" style="opacity:0.6;"></div>
+            
+            <h1 style="z-index:3;font-size:clamp(12px, 10vw, 64px);">Software</h1> <!-- clamp(min,max,preferred)  --> 
+            <p style="z-index:3;color:rgb(94,144,253);margin-top:-10px;font-size:clamp(6px, 5vw, 32px);"></p>
+        </div>
+        
+        
+        <div class="main">
+            
+            
+            <!----------------- software 1 --------------------------------->
+            
+            <div class="container" style="height:100%;width:clamp(350px, 70vw, 800px);text-align:left;">
+                    
+                <a href="https://vldbsolutions.com/tools/dbsee-teradata-health-sql/" style="text-decoration:none;">
+                    <h1 style="margin-top:50px;color:rgb(94,144,253);font-size:clamp(8px, 6vw, 36px);">DBSee</h1>
+                </a>
+                
+                <div class="divider"></div></br>
+                <i>VLDB Solutions</i></br></br>
+                
+                <ul>
+                    <a href="https://vldbsolutions.com/tools/dbsee-teradata-health-sql/" style="text-decoration:none;color:white;">
+                        <li>
+                        <blue>Teradata</blue> system health-checker that makes use of the Teradata data dictionary.<blue></li></br>
+                    </a>
+                </ul>
+    
+                <a href="https://vldbsolutions.com/tools/dbsee-teradata-health-sql/" style="text-decoration:none;color:white;">
+                    <div class="image" style="background-image:url('/images/dbsee.jpg');height:clamp(200px,300px,400px);width:clamp(200px,300px,400px);margin-top:15px;margin:auto;">
+                    </div>
+                </a>
+                    
+                </br></br>
+                
+            </div>
+            
+            
+ 
+            <!----------------- software 2 --------------------------------->
+            
+            <div class="list_wrapper" style="margin-top:2.5%;margin-bottom:5%;">
+                <h1 style="margin-top:50px;color:rgb(94,144,253);font-size:clamp(8px, 6vw, 36px);">Coming soon...</h1>
+                <div class="divider"></div></br>
+                <ul>
+                    <li>More content is on the way. Check back soon! <div style="color:rgb(13,20,28);">_______________________________________________________________________________</div></li>
+                </ul>
+            </div>
+            </br></br>
+
+        </div>
+        
+<!------------------------------------------ footer ----------------------------------->
+        <div class="footer"> 
+            </br></br>
+            <div class="container" style="max-width:80%;">
+                <p style="text-align:center;">
+                Any content produced as part of an employment has been displayed with permission of the employer.</br></br>
+                Copyright 2024.
+                </p> 
+            </div>
+        </div>
+    </body>
+    
+</html>
